af6c950416b34f8775c988cc2e9858ff47d4b46d
Shabib Hossain <shabib87@users.noreply.github.com>
2020-07-19 19:46:20 -0400

    trying to download license from github

--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -4,13 +4,17 @@ import Combine
 
 let fileManager = FileManager.default
 
+enum SessionError: Error {
+    case statusCode(HTTPURLResponse)
+}
+
 // MARK: xcode spm
 // Get .xcworkspace name from console input/yml config file
 // Get Package.resolved from .xcworkspace
 // read package for github url
 // fetch license from github
 //$PROJECT_FILE_PATH/project.xcworkspace/xcshareddata/swiftpm/
-func abc() {
+func getLicenseFromXcodeManagedSPM() {
     do {
         var path = "\(fileManager.currentDirectoryPath)"
         if let projName = path.components(separatedBy: "/").last {
@@ -31,7 +35,7 @@ func abc() {
                 }
 
                 do {
-                    let obj = try JSONDecoder().decode(Obbb.self, from: jsonData)
+                    let obj = try JSONDecoder().decode(WrappedPackageItems.self, from: jsonData)
                     print("got value man")
                     
                     obj.object.pins.forEach { pin in
@@ -50,6 +54,15 @@ func abc() {
                             }
                             
                             cancellable = URLSession.shared.dataTaskPublisher(for: url)
+                                .tryMap { data, response -> Data in
+                                    if let response = response as? HTTPURLResponse,
+                                        (200..<300).contains(response.statusCode) == false {
+                                        throw SessionError.statusCode(response)
+                                    }
+
+                                    return data
+                                }
+                            .decode(type: LicenseInfo.self, decoder: JSONDecoder())
                                 .sink(receiveCompletion: {
                                     switch $0 {
                                     case .finished:
@@ -59,8 +72,14 @@ func abc() {
                                         exit(EXIT_FAILURE)
                                     }
                                 }, receiveValue: {
-                                    let responseText = String(data: $0.data, encoding: .utf8)!
-                                    print(responseText)
+                                    print("Recieved download url")
+                                    print($0.downloadUrl)
+                                    
+                                    guard let url = URL(string: $0.downloadUrl) else {
+                                        print("invalid download url")
+                                        return
+                                    }
+                                    Downloader.shared.download(from: url)
                                 })
 
                             RunLoop.current.run()
@@ -74,174 +93,154 @@ func abc() {
         }
     } catch {
         print("coundn't read files: \(error)")
-        //    exit(EXIT_FAILURE)
     }
 }
 
-abc()
-
 var cancellable: AnyCancellable?
 
-//func def() {
-//    let json = """
-//{
-//  "object": {
-//    "pins": [
-//      {
-//        "package": "ViewInspector",
-//        "repositoryURL": "https://github.com/nalexn/ViewInspector",
-//        "state": {
-//          "branch": null,
-//          "revision": "7a672b0a4c730d829ace40918bd65c21c2f356d9",
-//          "version": "0.4.0"
-//        }
-//      }
-//    ]
-//  },
-//  "version": 1
-//}
-//"""
-//    
-//    guard let jsonData = json.data(using: .utf8) else {
-//        print("could not make json data from string")
-//        return
-//    }
-//
-//    do {
-//        let obj = try JSONDecoder().decode(Obbb.self, from: jsonData)
-//        print("got value man")
-//        obj.object.pins.forEach { pin in
-//            print(pin.repositoryURL)
-//            let repo = pin.repositoryURL.components(separatedBy: "/")
-//            
-//            if repo.count > 3 {
-//                let owner = repo[3]
-//                let repo = pin.package
-//                
-//                let urlStr = "https://api.github.com/repos/\(owner)/\(repo)/license"
-//                print(urlStr)
-//                
-//                guard let url = URL(string: urlStr) else {
-//                    return
-//                }
-//                
-//                cancellable = URLSession.shared.dataTaskPublisher(for: url)
-//                    .sink(receiveCompletion: {
-//                        switch $0 {
-//                        case .finished:
-//                            exit(EXIT_SUCCESS)
-//                        case let .failure(error):
-//                            print(error)
-//                            exit(EXIT_FAILURE)
-//                        }
-//                    }, receiveValue: {
-//                        let responseText = String(data: $0.data, encoding: .utf8)!
-//                        print(responseText)
-//                    })
-//
-//                RunLoop.current.run()
-//            }
-//        }
-//    } catch let error as NSError {
-//        print("Failed to load: \(error)")
-//    }
-//}
-
-//def()
-
-//let url1 = URL(string: "https://api.github.com/repos/nalexn/ViewInspector/license")!
-
-struct Obbb: Decodable {
+struct WrappedPackageItems: Decodable {
     let object: PackageItems
 }
 
-struct PackageItems: Codable {
+struct PackageItems: Decodable {
     let pins: [PackageObj]
 }
 
-struct PackageObj: Codable {
+struct PackageObj: Decodable {
     let package: String
     let repositoryURL: String
     let state: PackageObjState
 }
 
-struct PackageObjState: Codable {
+struct PackageObjState: Decodable {
     let version: String
+}
+
+struct LicenseInfo: Decodable {
+    let downloadUrl: String
 }
 
 
 // MARK: SPM
-//do {
-//    let root = try fileManager.contentsOfDirectory(atPath: ".")
-//    var isDir : ObjCBool = false
-//    if root.contains(".build"),
-//        fileManager.fileExists(atPath: ".build", isDirectory:&isDir)
-//        {
-//            if isDir.boolValue {
-//                let spm = try fileManager.contentsOfDirectory(atPath: ".build/")
-//                if spm.contains("checkouts"),
-//                    fileManager.fileExists(atPath: ".build/checkouts", isDirectory:&isDir), isDir.boolValue {
-//                    let checkouts = try fileManager.contentsOfDirectory(atPath: ".build/checkouts")
-//                    try checkouts.forEach { package in
-//                        print(">>> \(package)")
-//                        let files = try fileManager.contentsOfDirectory(atPath: ".build/checkouts/\(package)")
-//                        try files.forEach { file in
-//                            if file.contains("LICENSE"),
-//                                fileManager.fileExists(atPath: ".build/checkouts", isDirectory:&isDir), isDir.boolValue {
-//                                let url = URL(fileURLWithPath: ".build/checkouts/\(package)/\(file)")
-//                                let license = try String(contentsOf: url, encoding: .utf8)
-//                                print(license)
-//                            }
-//                        }
-//                        print(">>>")
-//                    }
-//                }
-//            }
-//    }
-//} catch {
-//    print("coundn't read files: \(error.localizedDescription)")
-//    exit(EXIT_FAILURE)
-//}
+func getLicenseFromLocalSPM() {
+    do {
+        let root = try fileManager.contentsOfDirectory(atPath: ".")
+        var isDir : ObjCBool = false
+        if root.contains(".build"),
+            fileManager.fileExists(atPath: ".build", isDirectory:&isDir)
+            {
+                if isDir.boolValue {
+                    let spm = try fileManager.contentsOfDirectory(atPath: ".build/")
+                    if spm.contains("checkouts"),
+                        fileManager.fileExists(atPath: ".build/checkouts", isDirectory:&isDir), isDir.boolValue {
+                        let checkouts = try fileManager.contentsOfDirectory(atPath: ".build/checkouts")
+                        try checkouts.forEach { package in
+                            print(">>> \(package)")
+                            let files = try fileManager.contentsOfDirectory(atPath: ".build/checkouts/\(package)")
+                            try files.forEach { file in
+                                if file.contains("LICENSE"),
+                                    fileManager.fileExists(atPath: ".build/checkouts", isDirectory:&isDir), isDir.boolValue {
+                                    let url = URL(fileURLWithPath: ".build/checkouts/\(package)/\(file)")
+                                    let license = try String(contentsOf: url, encoding: .utf8)
+                                    print(license)
+                                }
+                            }
+                            print(">>>")
+                        }
+                    }
+                }
+        }
+    } catch {
+        print("coundn't read files: \(error.localizedDescription)")
+        exit(EXIT_FAILURE)
+    }
+}
 
 // MARK: POD
 
-//do {
-//    let root = try fileManager.contentsOfDirectory(atPath: ".")
-//    var isDir : ObjCBool = false
-//
-//    if root.contains("Pods"),
-//        fileManager.fileExists(atPath: "Pods", isDirectory:&isDir) {
-//        if isDir.boolValue {
-//            // file exists and is a directory
-//            let pods = try fileManager.contentsOfDirectory(atPath: "Pods/")
-//            try pods.forEach { pod in
-//                if fileManager.fileExists(atPath: "Pods/\(pod)", isDirectory: &isDir) {
-//                    if isDir.boolValue {
-//                        // file exists and is a directory
-//                        print("<<<")
-//                        print(pod)
-//                        let files = try fileManager.contentsOfDirectory(atPath: "Pods/\(pod)")
-//                        try files.forEach { file in
-//                            if fileManager.fileExists(atPath: "Pods/\(pod)/\(file)", isDirectory:&isDir) {
-//                                if !isDir.boolValue,
-//                                 file.contains("LICENSE") {
-//                                    let url = URL(fileURLWithPath: "Pods/\(pod)/\(file)")
-//                                    let license = try String(contentsOf: url, encoding: .utf8)
-//                                    print(license)
-//                                }
-//                            }
-//                        }
-//                        print(">>>")
-//                    }
-//                }
-//            }
-//        } else {
-//            // file exists and is not a directory
-//        }
-//    } else {
-//        // file does not exist
-//    }
-//} catch {
-//    print("coundn't read files: \(error.localizedDescription)")
-//    exit(EXIT_FAILURE)
-//}
+func getLicensenseFromCocopod() {
+    do {
+        let root = try fileManager.contentsOfDirectory(atPath: ".")
+        var isDir : ObjCBool = false
+
+        if root.contains("Pods"),
+            fileManager.fileExists(atPath: "Pods", isDirectory:&isDir) {
+            if isDir.boolValue {
+                // file exists and is a directory
+                let pods = try fileManager.contentsOfDirectory(atPath: "Pods/")
+                try pods.forEach { pod in
+                    if fileManager.fileExists(atPath: "Pods/\(pod)", isDirectory: &isDir) {
+                        if isDir.boolValue {
+                            // file exists and is a directory
+                            print("<<<")
+                            print(pod)
+                            let files = try fileManager.contentsOfDirectory(atPath: "Pods/\(pod)")
+                            try files.forEach { file in
+                                if fileManager.fileExists(atPath: "Pods/\(pod)/\(file)", isDirectory:&isDir) {
+                                    if !isDir.boolValue,
+                                     file.contains("LICENSE") {
+                                        let url = URL(fileURLWithPath: "Pods/\(pod)/\(file)")
+                                        let license = try String(contentsOf: url, encoding: .utf8)
+                                        print(license)
+                                    }
+                                }
+                            }
+                            print(">>>")
+                        }
+                    }
+                }
+            } else {
+                // file exists and is not a directory
+            }
+        } else {
+            // file does not exist
+        }
+    } catch {
+        print("coundn't read files: \(error.localizedDescription)")
+        exit(EXIT_FAILURE)
+    }
+}
+
+final class Downloader: NSObject, URLSessionDownloadDelegate {
+    static let shared = Downloader()
+    
+    private override init() {}
+    
+    func download(from url: URL) {
+        let configuration = URLSessionConfiguration.default
+        let operationQueue = OperationQueue()
+        let session = URLSession(configuration: configuration,
+                                 delegate: self,
+                                 delegateQueue: operationQueue)
+        
+        let downloadTask = session.downloadTask(with: url)
+        downloadTask.resume()
+        RunLoop.current.run()
+    }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        
+        // get downloaded data from location
+        do {
+            let license = try String(contentsOf: location, encoding: .utf8)
+            print(license)
+        } catch {
+            print("coundn't read files: \(error.localizedDescription)")
+            exit(EXIT_FAILURE)
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        let percentDownloaded = totalBytesWritten / totalBytesExpectedToWrite
+        
+        // update the percentage label
+        DispatchQueue.main.async {
+            print(" downloaded: \(percentDownloaded * 100)%")
+        }
+    }
+}
+
+getLicenseFromXcodeManagedSPM()
+getLicenseFromLocalSPM()
+getLicensenseFromCocopod()
