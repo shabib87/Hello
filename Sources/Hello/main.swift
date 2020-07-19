2dc32f28ce3a30a2df27e6496dd51252c9af1ba5
Shabib Hossain <shabib87@users.noreply.github.com>
2020-07-18 22:16:23 -0400

    printing pod files

--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -34,12 +34,49 @@ do {
 
 do {
     let root = try fileManager.contentsOfDirectory(atPath: ".")
-    if root.contains("Pods") {
-        let pods = try fileManager.contentsOfDirectory(atPath: "Pods/")
+    var isDir : ObjCBool = false
+    
+    if root.contains("Pods"),
+        fileManager.fileExists(atPath: "Pods", isDirectory:&isDir) {
+        if isDir.boolValue {
+            // file exists and is a directory
+            let pods = try fileManager.contentsOfDirectory(atPath: "Pods/")
+            try pods.forEach { pod in
+                if fileManager.fileExists(atPath: "Pods/\(pod)", isDirectory: &isDir) {
+                    if isDir.boolValue {
+                        // file exists and is a directory
+                        print("<<<")
+                        print(pod)
+                        let files = try fileManager.contentsOfDirectory(atPath: "Pods/\(pod)")
+                        try files.forEach { file in
+                            if fileManager.fileExists(atPath: "Pods/\(pod)/\(file)", isDirectory:&isDir) {
+                                if !isDir.boolValue,
+                                 file.contains("LICENSE") {
+                                    let url = URL(fileURLWithPath: "Pods/\(pod)/\(file)")
+                                    let license = try String(contentsOf: url, encoding: .utf8)
+                                    print(license)
+                                }
+                            }
+                        }
+                        print(">>>")
+                    }
+                }
+            }
+        } else {
+            // file exists and is not a directory
+        }
+    } else {
+        // file does not exist
+    }
+    
+//    if root.contains("Pods") {
+//        let pods = try fileManager.contentsOfDirectory(atPath: "Pods/")
 //        try
-            pods.forEach { pod in
+//            pods.forEach { pod in
+//            guard pod != ".DS_Store" else { return }
 //            let files = try fileManager.contentsOfDirectory(atPath: "Pods/\(pod)")
-            print(">>> \(pod)")
+//
+//            print(">>> \(pod)")
 //            try
 //                files.forEach { file in
 //                if file.contains("LICENSE") {
@@ -49,9 +86,9 @@ do {
 //                }
 //                    print(file)
 //            }
-            print(">>>")
-        }
-    }
+//            print(">>>")
+//        }
+//    }
 } catch {
     print("coundn't read files: \(error.localizedDescription)")
     exit(EXIT_FAILURE)
