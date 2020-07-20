1d3b25be14a4d06eda566dbb6e92e66afe8ecebe
Shabib Hossain <shabib87@users.noreply.github.com>
2020-07-19 20:34:42 -0400

    successfully downloading license file

--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -9,11 +9,6 @@ enum SessionError: Error {
 }
 
 // MARK: xcode spm
-// Get .xcworkspace name from console input/yml config file
-// Get Package.resolved from .xcworkspace
-// read package for github url
-// fetch license from github
-//$PROJECT_FILE_PATH/project.xcworkspace/xcshareddata/swiftpm/
 func getLicenseFromXcodeManagedSPM() {
     do {
         var path = "\(fileManager.currentDirectoryPath)"
@@ -69,7 +64,7 @@ func getLicenseFromXcodeManagedSPM() {
                                 .sink(receiveCompletion: {
                                     switch $0 {
                                     case .finished:
-                                        exit(EXIT_SUCCESS)
+                                        print("task complete")
                                     case let .failure(error):
                                         print(error)
                                         exit(EXIT_FAILURE)
@@ -217,33 +212,135 @@ final class Downloader: NSObject, URLSessionDownloadDelegate {
                                  delegateQueue: operationQueue)
         
         let downloadTask = session.downloadTask(with: url)
+        
         downloadTask.resume()
         RunLoop.current.run()
     }
+    
+    func urlSession(_ session: URLSession,
+                    didBecomeInvalidWithError error: Error?) {
+        print(error as Any)
+    }
+    
+    func urlSession(_ session: URLSession,
+                    task: URLSessionTask,
+                    didCompleteWithError error: Error?) {
+        print(error as Any)
+    }
 
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        
-        // get downloaded data from location
+    func urlSession(_ session: URLSession,
+                    downloadTask: URLSessionDownloadTask,
+                    didFinishDownloadingTo location: URL) {
         do {
             let license = try String(contentsOf: location, encoding: .utf8)
             print(license)
+            exit(EXIT_SUCCESS)
         } catch {
             print("coundn't read files: \(error.localizedDescription)")
             exit(EXIT_FAILURE)
         }
     }
     
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+    func urlSession(_ session: URLSession,
+                    downloadTask: URLSessionDownloadTask,
+                    didWriteData bytesWritten: Int64,
+                    totalBytesWritten: Int64,
+                    totalBytesExpectedToWrite: Int64) {
         
         let percentDownloaded = totalBytesWritten / totalBytesExpectedToWrite
-        
-        // update the percentage label
         DispatchQueue.main.async {
-            print(" downloaded: \(percentDownloaded * 100)%")
+            print("downloaded \(percentDownloaded * 100)%")
         }
     }
 }
 
-getLicenseFromXcodeManagedSPM()
+final class TestLicenseDownload: NSObject {
+    func testing() {
+        let json = """
+    {
+      "object": {
+        "pins": [
+          {
+            "package": "ViewInspector",
+            "repositoryURL": "https://github.com/nalexn/ViewInspector",
+            "state": {
+              "branch": null,
+              "revision": "7a672b0a4c730d829ace40918bd65c21c2f356d9",
+              "version": "0.4.0"
+            }
+          }
+        ]
+      },
+      "version": 1
+    }
+    """
+
+        guard let jsonData = json.data(using: .utf8) else {
+            print("could not make json data from string")
+            return
+        }
+
+        do {
+            let obj = try JSONDecoder().decode(WrappedPackageItems.self, from: jsonData)
+            print("got value man")
+            obj.object.pins.forEach { pin in
+                print(pin.repositoryURL)
+                let repo = pin.repositoryURL.components(separatedBy: "/")
+
+                if repo.count > 3 {
+                    let owner = repo[3]
+                    let repo = pin.package
+
+                    let urlStr = "https://api.github.com/repos/\(owner)/\(repo)/license"
+                    print(urlStr)
+
+                    guard let url = URL(string: urlStr) else {
+                        return
+                    }
+                    
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    
+                    cancellable = URLSession.shared.dataTaskPublisher(for: url)
+                        .tryMap { data, response -> Data in
+                            if let response = response as? HTTPURLResponse,
+                                (200..<300).contains(response.statusCode) == false {
+                                throw SessionError.statusCode(response)
+                            }
+
+                            return data
+                        }
+                    .decode(type: LicenseInfo.self, decoder: decoder)
+                        .sink(receiveCompletion: {
+                            switch $0 {
+                            case .finished:
+                                print("task complete")
+                            case let .failure(error):
+                                print(error)
+                                exit(EXIT_FAILURE)
+                            }
+                        }, receiveValue: {
+                            print("Recieved download url")
+                            print($0.downloadUrl)
+                            
+                            guard let url = URL(string: $0.downloadUrl) else {
+                                print("invalid download url")
+                                return
+                            }
+                            
+                            Downloader.shared.download(from: url)
+                        })
+
+                    RunLoop.current.run()
+                }
+            }
+        } catch let error as NSError {
+            print("Failed to load: \(error)")
+        }
+    }
+}
+
 getLicenseFromLocalSPM()
 getLicensenseFromCocopod()
+getLicenseFromXcodeManagedSPM()
+//TestLicenseDownload().testing()
